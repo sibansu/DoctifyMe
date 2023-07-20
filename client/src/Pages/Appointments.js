63f85f23f61212ca8497c09f8ac1058d4a14b941
Sibansu <mishrasibansu16@gmail.com>
2023-07-20 23:28:47 +0530

    appointment table update

--- client/src/Pages/Appointments.js
+++ client/src/Pages/Appointments.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Layout from '../Components/Layout'
 import axios from 'axios'
-
+import moment from 'moment'
+import { Table } from 'antd'
 function Appointments() {
 
   const [appointments, setAppointments] = useState([])
-  const getAppointments = async()=>{
+  const getAppointments = async () => {
     try {
-      const res = await axios.get('/api/v1/user/user-appointments',{
-        headers:{
+      const res = await axios.get('/api/v1/user/user-appointments', {
+        headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
       })
 
-      if(res.data.success){
+      if (res.data.success) {
         setAppointments(res.data.data)
       }
     } catch (error) {
@@ -24,12 +25,50 @@ function Appointments() {
   useEffect(() => {
     getAppointments()
   }, [])
-  
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id"
+    },
+    // {
+    //   title: 'Name',
+    //   dataIndex: 'name',
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorId.firstName} {record.doctorId.lastName}
+    //     </span>
+    //   )
+    // },
+    // {
+    //   title: 'Phone',
+    //   dataIndex: 'phone',
+    //   render: (text, record) => (
+    //     <span>
+    //       {record.doctorId.phone}
+    //     </span>
+    //   )
+    // },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format('DD-MM-YYYY')}
+        </span>
+      )
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+    }
+  ]
 
   const [first, setfirst] = useState()
   return (
     <Layout>
-        <h3>Appointments List</h3>
+      <h3>Appointments List</h3>
+      <Table columns={columns} dataSource={appointments}></Table>
     </Layout>
   )
 }
