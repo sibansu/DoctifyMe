4cd4401b3a6852e1edf8e144c83c67559d62a3ae
Sibansu <mishrasibansu16@gmail.com>
2023-07-31 22:23:40 +0530

    build ready

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors= require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require('path')
 
 dotenv.config()
 
